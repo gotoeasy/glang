8d32049ad74b8c806e3851c84caf69d65f3315a5
gotoeasy <gotoeasy@163.com>
2022-11-04 13:08:00 +0800

    Random

--- cmn/number_util.go
+++ cmn/number_util.go
@@ -2,7 +2,9 @@ package cmn
 
 import (
 	"encoding/binary"
+	"math/rand"
 	"strconv"
+	"time"
 )
 
 func IntToStr(i int) string {
@@ -17,4 +19,15 @@ func Uint32ToBytes(num uint32) []byte {
 
 func BytesToUint32(bytes []byte) uint32 {
 	return uint32(binary.BigEndian.Uint32(bytes))
+}
+
+func RandomInt(min, max int) int {
+	if min == max {
+		return min
+	}
+	if max < min {
+		min, max = max, min
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(max-min) + min
 }
