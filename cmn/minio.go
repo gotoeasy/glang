cdcad3aa233acf93ff95c27eb5f11061590fbe01
gotoeasy <gotoeasy@163.com>
2022-11-04 20:16:00 +0800

    Minio

--- cmn/minio.go
+++ cmn/minio.go
@@ -9,10 +9,19 @@ import (
 
 // Minio客户端结构体
 type Minio struct {
-	Endpoint string
-	Username string
-	Password string
-	Bucket   string
+	endpoint string
+	username string
+	password string
+	bucket   string
+}
+
+func NewMinio(endpoint string, username string, password string, bucket string) *Minio {
+	return &Minio{
+		endpoint: endpoint,
+		username: username,
+		password: password,
+		bucket:   bucket,
+	}
 }
 
 // 上传文件
@@ -20,22 +29,22 @@ func (m *Minio) Upload(localPathFile string, minioObjectName string) error {
 	ctx := context.Background()
 
 	// 初始化
-	minioClient, err := minio.New(m.Endpoint, &minio.Options{Creds: credentials.NewStaticV4(m.Username, m.Password, ""), Secure: false})
+	minioClient, err := minio.New(m.endpoint, &minio.Options{Creds: credentials.NewStaticV4(m.username, m.password, ""), Secure: false})
 	if err != nil {
 		return err
 	}
 
 	// 检查创建Bucket
-	exists, err := minioClient.BucketExists(ctx, m.Bucket)
+	exists, err := minioClient.BucketExists(ctx, m.bucket)
 	if !(err == nil && exists) {
-		err = minioClient.MakeBucket(ctx, m.Bucket, minio.MakeBucketOptions{})
+		err = minioClient.MakeBucket(ctx, m.bucket, minio.MakeBucketOptions{})
 		if err != nil {
 			return err
 		}
 	}
 
 	// 上传
-	_, err = minioClient.FPutObject(ctx, m.Bucket, minioObjectName, localPathFile, minio.PutObjectOptions{})
+	_, err = minioClient.FPutObject(ctx, m.bucket, minioObjectName, localPathFile, minio.PutObjectOptions{})
 	if err != nil {
 		return err
 	}
@@ -48,13 +57,13 @@ func (m *Minio) Download(minioObjectName string, localPathFile string) error {
 	ctx := context.Background()
 
 	// 初始化
-	minioClient, err := minio.New(m.Endpoint, &minio.Options{Creds: credentials.NewStaticV4(m.Username, m.Password, ""), Secure: false})
+	minioClient, err := minio.New(m.endpoint, &minio.Options{Creds: credentials.NewStaticV4(m.username, m.password, ""), Secure: false})
 	if err != nil {
 		return err
 	}
 
 	// 下载
-	err = minioClient.FGetObject(ctx, m.Bucket, minioObjectName, localPathFile, minio.GetObjectOptions{})
+	err = minioClient.FGetObject(ctx, m.bucket, minioObjectName, localPathFile, minio.GetObjectOptions{})
 	if err != nil {
 		return err
 	}
