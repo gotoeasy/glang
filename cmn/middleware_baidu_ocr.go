905a3029a9a5a196abfc2513ad15dd039bbd50fb
gotoeasy <gotoeasy@163.com>
2022-11-06 11:11:39 +0800

    百度接口发票识别

--- cmn/middleware_baidu_ocr.go
+++ cmn/middleware_baidu_ocr.go
@@ -1,24 +1,25 @@
 package cmn
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"net/http"
 	"net/url"
-	"strings"
 	"time"
+
+	"github.com/nfnt/resize"
 )
 
 type baiduToken struct {
-	//	RefreshToken  string `json:"refresh_token,omitempty"`
-	ExpiresIn int64 `json:"expires_in,omitempty"` // 默认2592000秒，即30天
-	//	SessionKey  string `json:"session_key,omitempty"`
-	AccessToken string `json:"access_token,omitempty"`
-	Expire      int64  `json:"expire,omitempty"` // 最大有效时间（秒）
-	//	Scope         string `json:"scope,omitempty"`
-	//	SessionSecret string `json:"session_secret,omitempty"`
+	ExpiresIn   int64  `json:"expires_in,omitempty"`   // 默认2592000秒，即30天
+	AccessToken string `json:"access_token,omitempty"` // 令牌
+	Expire      int64  `json:"expire,omitempty"`       // 最大有效时间（秒）
 }
 
 type BaiduOcr struct {
@@ -35,7 +36,7 @@ func NewBaiduOcr(apiKey string, secretKey string) *BaiduOcr {
 	}
 }
 
-// 定额发票识别（返回JSON识别结果）
+// 定额发票识别，支持png/jpeg/jpg/bpm/pdf类型（返回JSON识别结果）
 /*
  支持对各类定额发票的发票代码、发票号码、金额、发票所在地、发票金额小写、省、市7个关键字段进行结构化识别
 */
@@ -53,7 +54,7 @@ func (b *BaiduOcr) QuotaInvoice(filebytes []byte, isPdf bool) (string, error) {
 	b64str := Base64(filebytes)
 	if len(filebytes) > 4*1000*1000 || len(b64str) > 4000*1000 {
 		Debug("尝试压缩图片，压缩前", len(filebytes), len(b64str))
-		filebytes = ImageCompress(filebytes, 3072, 3072, nil) //  修改图片的大小(最大3072，百度接口base64编码后小于4M，分辨率不高于4096*4096)
+		filebytes = b.compressImg(filebytes)
 		b64str = Base64(filebytes)
 		Debug("尝试压缩图片，压缩后", len(filebytes), len(b64str))
 	}
@@ -61,36 +62,16 @@ func (b *BaiduOcr) QuotaInvoice(filebytes []byte, isPdf bool) (string, error) {
 		return "", errors.New("文件太大了")
 	}
 
-	sendBody := http.Request{}
-	sendBody.ParseForm()
+	formMap := make(map[string]string)
 	if isPdf {
-		sendBody.Form.Add("pdf_file", b64str)
+		formMap["pdf_file"] = b64str
 	} else {
-		sendBody.Form.Add("image", b64str)
+		formMap["image"] = b64str
 	}
-	sendData := sendBody.Form.Encode()
-
-	client := &http.Client{}
-	request, err := http.NewRequest("POST", uri.String(), strings.NewReader(sendData))
-	if err != nil {
-		return "", err
-	}
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	response, err := client.Do(request)
-	if err != nil {
-		return "", err
-	}
-	defer response.Body.Close()
-	result, err := io.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return BytesToString(result), nil
+	return HttpPostForm(uri.String(), formMap)
 }
 
-// 增值税发票识别（返回JSON识别结果）
+// 增值税发票识别，支持png/jpeg/jpg/bpm/pdf类型（返回JSON识别结果）
 /*
 支持对增值税普票、专票、卷票、电子发票的所有字段进行结构化识别，
 包括发票基本信息、销售方及购买方信息、商品信息、价税信息等，其中四要素识别准确率超过 99.9%；
@@ -113,7 +94,7 @@ func (b *BaiduOcr) VatInvoice(filebytes []byte, isPdf bool) (string, error) {
 	b64str := base64.StdEncoding.EncodeToString(filebytes)
 	if len(filebytes) > 4*1000*1000 || len(b64str) > 4000*1000 {
 		Debug("尝试压缩图片，压缩前", len(filebytes), len(b64str))
-		filebytes = ImageCompress(filebytes, 3072, 3072, nil) //  修改图片的大小(最大3072，百度接口base64编码后小于4M，分辨率不高于4096*4096)
+		filebytes = b.compressImg(filebytes)
 		b64str = base64.StdEncoding.EncodeToString(filebytes)
 		Debug("尝试压缩图片，压缩后", len(filebytes), len(b64str))
 	}
@@ -121,33 +102,13 @@ func (b *BaiduOcr) VatInvoice(filebytes []byte, isPdf bool) (string, error) {
 		return "", errors.New("文件太大了")
 	}
 
-	sendBody := http.Request{}
-	sendBody.ParseForm()
+	formMap := make(map[string]string)
 	if isPdf {
-		sendBody.Form.Add("pdf_file", b64str)
+		formMap["pdf_file"] = b64str
 	} else {
-		sendBody.Form.Add("image", b64str)
+		formMap["image"] = b64str
 	}
-	sendData := sendBody.Form.Encode()
-
-	client := &http.Client{}
-	request, err := http.NewRequest("POST", uri.String(), strings.NewReader(sendData))
-	if err != nil {
-		return "", err
-	}
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	response, err := client.Do(request)
-	if err != nil {
-		return "", err
-	}
-	defer response.Body.Close()
-	result, err := io.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return BytesToString(result), nil
+	return HttpPostForm(uri.String(), formMap)
 }
 
 // 取令牌
@@ -208,4 +169,37 @@ func (b *BaiduOcr) initAccessToken() {
 
 	tk.Expire = time.Now().Unix() + tk.ExpiresIn - 60
 	b.token = tk
+}
+
+func (b *BaiduOcr) compressImg(buf []byte) []byte {
+
+	// 文件压缩
+	img, layout, err := image.Decode(bytes.NewReader(buf))
+	if err != nil {
+		Error(err)
+		return buf
+	}
+
+	// 修改图片的大小(最大3072，百度接口base64编码后小于4M，分辨率不高于4096*4096)
+	set := resize.Thumbnail(3072, 3072, img, resize.Lanczos3) // Lanczos3 算法文件最大，图片最清晰，NearestNeighbor 最差
+	newBuf := bytes.Buffer{}
+	switch layout {
+	case "png":
+		err = png.Encode(&newBuf, set)
+	case "jpeg", "jpg":
+		err = jpeg.Encode(&newBuf, set, &jpeg.Options{Quality: 80})
+	case "bmp":
+		buf = BmpToJpg(buf, nil)
+	default:
+		Error("暂不支持该文件压缩")
+		return buf
+	}
+	if err != nil {
+		Error(err)
+		return buf
+	}
+	if newBuf.Len() < len(buf) {
+		buf = newBuf.Bytes()
+	}
+	return buf
 }
