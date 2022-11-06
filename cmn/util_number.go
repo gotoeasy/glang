3564ad0b7f224334e4bc81e7c48386629ffc2ddf
gotoeasy <gotoeasy@163.com>
2022-11-06 15:48:31 +0800

    随机数

--- cmn/util_number.go
+++ cmn/util_number.go
@@ -16,3 +16,8 @@ func RandomInt(min, max int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r.Intn(max-min) + min
 }
+
+// 随机数
+func RandomUint32() uint32 {
+	return rand.New(rand.NewSource(time.Now().UnixNano())).Uint32()
+}
