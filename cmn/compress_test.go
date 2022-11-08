8ff94e860d5ef513f842485faa13549cb3d2d019
gotoeasy <gotoeasy@163.com>
2022-11-08 13:43:57 +0800

    snappy

--- cmn/compress_test.go
+++ cmn/compress_test.go
@@ -8,3 +8,16 @@ func Test_gzip(t *testing.T) {
 	Info(Gzip("f:\\222\\ddd.tar", "f:\\222\\ddd.tar.gz"))
 	Info(UnGzip("f:\\222\\ddd.tar.gz", "f:\\222\\dddsss"))
 }
+
+func Test_snappy(t *testing.T) {
+	src := `使用snappy算法压缩（压缩速度快，占用资源少，压缩比适当，重复多则压缩比大，适用于重复较多的文本压缩）`
+	src = `DEBUG ==> Parameters: 1589491593233129481(String), XFD(String), B3特菜档口(String), 003B307620221(String), SC-B3-076(String), SC(String), 王二军(String), 82107(String), 2022-09-22(String), 50400.0000000000(BigDecimal), 6(Integer), 收入-租金(String), 603084(String), 2023-02-01(String), 2023-02-28(String), 4200.0000000000(BigDecimal), 4200.0000000000(BigDecimal), 4000.0000000000(BigDecimal), 200.0000000000(BigDecimal), 5(String), 2023(String), 2(String), 2023-02-01~2023-02-28(String)`
+	src = `ERROR 处理TODO跟进人释放发生异常`
+
+	srcBytes := StringToBytes(src)
+	bt := Compress(srcBytes)
+	Info("压缩前字节长", len(srcBytes), "，压缩前后字节长", len(bt))
+
+	de := UnCompress(srcBytes)
+	Info(BytesToString(de) == src)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db
 	github.com/huichen/sego v0.0.0-20210824061530-c87651ea5c76
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 )
@@ -15,7 +16,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
