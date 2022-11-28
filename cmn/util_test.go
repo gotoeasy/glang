800c684d4685205ccb3b9d1ac49298a4c0512b5e
gotoeasy <gotoeasy@163.com>
2022-11-28 20:07:49 +0800

    ULID

--- cmn/util_test.go
+++ cmn/util_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+func Test_uid(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		Info(ULID())
+	}
+}
+
 func Test_decimal(t *testing.T) {
 	Info(FormatAmountRound(1, 3))
 	Info(FormatAmountRound(1, 0))
