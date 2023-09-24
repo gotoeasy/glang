356d8d143408b2f23c6bf22e46e1497cbd1a17a7
gotoeasy <gotoeasy@163.com>
2023-09-24 20:22:42 +0800

    down

---
+++ cmn/http_get_download.go
@@ -0,0 +1,28 @@
+package cmn
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// 下载文件
+func HttpDownload(url, saveAsPathFile string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	file, err := os.Create(saveAsPathFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
