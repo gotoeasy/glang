5f1e353125bffd4514e1da6f7875d7336869e8e8
gotoeasy <gotoeasy@163.com>
2023-10-03 12:45:13 +0800

    exit

--- cmn/os_signal_exit.go
+++ cmn/os_signal_exit.go
@@ -20,7 +20,17 @@ func init() {
 	}()
 }
 
-// 注册退出处理函数，在接收到SIGTERM或SIGINT信号时执行
+// 注册退出处理函数，在异步接收到SIGTERM或SIGINT信号时执行
 func OnExit(fnExit func()) {
 	exitFuncs = append(exitFuncs, fnExit)
+}
+
+// 同步等待到退出信号后退出
+func WaitExit(fnExit func()) {
+	osc := make(chan os.Signal, 1)
+	signal.Notify(osc, syscall.SIGTERM, syscall.SIGINT)
+	<-osc
+	if fnExit != nil {
+		fnExit()
+	}
 }
