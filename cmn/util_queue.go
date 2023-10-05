1396cbd9e49ed48c7054b06ae62322371259487c
gotoeasy <gotoeasy@163.com>
2023-10-05 16:19:33 +0800

    queue

--- cmn/util_queue.go
+++ cmn/util_queue.go
@@ -33,7 +33,7 @@ func (q *Queue) Push(item any) {
 func (q *Queue) Pop() any {
 	q.mu.Lock()
 	defer q.mu.Unlock()
-	if !q.IsEmpty() {
+	if len(q.items) != 0 {
 		item := q.items[0]
 		q.items = q.items[1:]
 		return item
@@ -45,7 +45,7 @@ func (q *Queue) Pop() any {
 func (q *Queue) Peek() any {
 	q.mu.Lock()
 	defer q.mu.Unlock()
-	if q.IsEmpty() {
+	if len(q.items) == 0 {
 		return nil
 	}
 	return q.items[0]
