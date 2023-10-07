bf9ebcc3d00f5b643b2e1c21562e79a10d35b6a9
gotoeasy <gotoeasy@163.com>
2023-10-07 12:29:51 +0800

    ip

--- cmn/os_general.go
+++ cmn/os_general.go
@@ -1,8 +1,14 @@
 package cmn
 
-import "net"
+import (
+	"net"
+	"os/exec"
+	"strings"
+	"syscall"
+)
 
 var localIpAddres string
+var localInternalIpAddres string
 var localHostName string
 
 // 取本机IP地址（IPv4）
@@ -16,10 +22,50 @@ func GetLocalIp() string {
 		for _, address := range addrs {
 			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
 				localIpAddres = ipnet.IP.String()
+				if Startwiths(localIpAddres, "172.") || Startwiths(localIpAddres, "192.") || Startwiths(localIpAddres, "10.") {
+					break // 内部IP优先
+				}
 			}
 		}
 	}
 	return localIpAddres
+}
+
+// 取本机IP地址（IPv4），eth0优先
+func GetPreferredLocalIPv4() string {
+	if localInternalIpAddres != "" {
+		return localInternalIpAddres // 用缓存
+	}
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		localInternalIpAddres = GetLocalIp()
+		return localInternalIpAddres
+	}
+
+	// 优先选择eth0接口的IPv4地址
+	for _, iface := range interfaces {
+		if strings.HasPrefix(iface.Name, "eth0") {
+			addrs, err := iface.Addrs()
+			if err != nil {
+				localInternalIpAddres = GetLocalIp()
+				return localInternalIpAddres
+			}
+
+			for _, addr := range addrs {
+				ipnet, ok := addr.(*net.IPNet)
+				if ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+					rs := ipnet.IP.String()
+					if Startwiths(rs, "172.") || Startwiths(rs, "192.") || Startwiths(rs, "10.") {
+						localInternalIpAddres = rs
+						return localInternalIpAddres // 理想情况下是从这里取到
+					}
+				}
+			}
+		}
+	}
+
+	localInternalIpAddres = GetLocalIp()
+	return localInternalIpAddres
 }
 
 // 取本机名
@@ -32,5 +78,20 @@ func GetLocalHostName() string {
 	if err != nil {
 		localHostName = info.Hostname
 	}
+
+	if localHostName == "" {
+		localHostName = getHostname()
+	}
 	return localHostName
+}
+
+func getHostname() string {
+	cmd := exec.Command("hostname")
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+	hostname := Trim(string(output))
+	return hostname
 }
