195406b4eeffe0bd97c4d5007ef15b436fc76d27
gotoeasy <gotoeasy@163.com>
2023-10-13 21:11:02 +0800

    header

--- cmn/http_get_download.go
+++ cmn/http_get_download.go
@@ -6,9 +6,23 @@ import (
 	"os"
 )
 
-// 下载文件
-func HttpDownload(url, saveAsPathFile string) error {
-	response, err := http.Get(url)
+// 下载文件，自定义headers格式为 K:V
+func HttpDownload(url, saveAsPathFile string, headers ...string) error {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	// 请求头
+	for i, max := 0, len(headers); i < max; i++ {
+		strs := Split(headers[i], ":")
+		if len(strs) > 1 {
+			req.Header.Set(Trim(strs[0]), Trim(strs[1]))
+		}
+	}
+
+	response, err := client.Do(req)
 	if err != nil {
 		return err
 	}
