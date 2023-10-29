87d1bd60a73350a0e85d3d683b51f1a9b776e5f6
gotoeasy <gotoeasy@163.com>
2023-10-29 10:58:00 +0800

    cache

---
+++ cmn/structure_cache_timeout.go
@@ -0,0 +1,74 @@
+package cmn
+
+import (
+	"sync"
+	"time"
+)
+
+// 缓存项结构
+type cacheItem struct {
+	value      any
+	expiration time.Time
+}
+
+// 缓存结构
+type Cache struct {
+	mu       sync.RWMutex
+	items    map[string]cacheItem
+	duration time.Duration
+}
+
+// 新建内存缓存(有存活期，会定期清理失效缓存)
+func NewCache(duration time.Duration) *Cache {
+	cache := &Cache{
+		items:    make(map[string]cacheItem),
+		duration: duration,
+	}
+	go cache.cleanupExpired()
+	return cache
+}
+
+// 添加缓存项
+func (c *Cache) Set(key string, value any) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	expiration := time.Now().Add(c.duration)
+	c.items[key] = cacheItem{value: value, expiration: expiration}
+}
+
+// 获取缓存项
+func (c *Cache) Get(key string) (any, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	item, found := c.items[key]
+	if !found {
+		return nil, false
+	}
+	if time.Now().After(item.expiration) {
+		c.Delete(key)
+		return nil, false
+	}
+	return item.value, true
+}
+
+// 删除缓存项
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.items, key)
+}
+
+// 每5分钟检查清理一次过期缓存项
+func (c *Cache) cleanupExpired() {
+	ticker := time.NewTicker(time.Minute * 5)
+	for {
+		<-ticker.C
+		c.mu.Lock()
+		for key, item := range c.items {
+			if time.Now().After(item.expiration) {
+				delete(c.items, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
