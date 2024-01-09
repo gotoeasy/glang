7000084d47bfe9bb17e215a2dc4f1db116577987
gotoeasy <gotoeasy@163.com>
2024-01-09 12:41:13 +0800

    按顺序取本机IP地址（IPv4）

--- cmn/os_general.go
+++ cmn/os_general.go
@@ -7,38 +7,65 @@ import (
 )
 
 var localIpAddres string
-var localInternalIpAddres string
 var localHostName string
 
-// 取本机IP地址（IPv4）
+// 按顺序取本机IP地址（IPv4），优先度 eth0 ip > 192.* > 172.* > 10.* > 其他
 func GetLocalIp() string {
+	return getPreferredLocalIPv4()
+}
+
+// 取本机IP地址（IPv4），优先度 192.* > 172.* > 10.* > 其他
+func getLocalIpAddres() string {
 	if localIpAddres != "" {
 		return localIpAddres
 	}
 
 	addrs, err := net.InterfaceAddrs()
 	if err == nil {
+		ip192, ip172, ip10, ipOther := "", "", "", ""
 		for _, address := range addrs {
 			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
-				localIpAddres = ipnet.IP.String()
-				if Startwiths(localIpAddres, "172.") || Startwiths(localIpAddres, "192.") || Startwiths(localIpAddres, "10.") {
-					break // 内部IP优先
+				ip := ipnet.IP.String()
+				if Startwiths(ip, "192.") {
+					ip192 = ip
+					break // 192.*的IP优先
+				}
+				if Startwiths(ip, "172.") {
+					ip172 = ip
+				} else if Startwiths(ip, "10.") {
+					ip10 = ip
+				} else if ipOther != "" {
+					ipOther = ip
 				}
 			}
+		}
+
+		// 192.* > 172.* > 10.* > 其他
+		if ipOther != "" {
+			localIpAddres = ipOther
+		}
+		if ip10 != "" {
+			localIpAddres = ip10
+		}
+		if ip172 != "" {
+			localIpAddres = ip172
+		}
+		if ip192 != "" {
+			localIpAddres = ip192
 		}
 	}
 	return localIpAddres
 }
 
-// 取本机IP地址（IPv4），eth0优先
-func GetPreferredLocalIPv4() string {
-	if localInternalIpAddres != "" {
-		return localInternalIpAddres // 用缓存
+// 取本机IP地址（IPv4），优先度 eth0 ip > 192.* > 172.* > 10.* > 其他
+func getPreferredLocalIPv4() string {
+	if localIpAddres != "" {
+		return localIpAddres // 用缓存
 	}
 	interfaces, err := net.Interfaces()
 	if err != nil {
-		localInternalIpAddres = GetLocalIp()
-		return localInternalIpAddres
+		localIpAddres = getLocalIpAddres()
+		return localIpAddres
 	}
 
 	// 优先选择eth0接口的IPv4地址
@@ -46,8 +73,8 @@ func GetPreferredLocalIPv4() string {
 		if strings.HasPrefix(iface.Name, "eth0") {
 			addrs, err := iface.Addrs()
 			if err != nil {
-				localInternalIpAddres = GetLocalIp()
-				return localInternalIpAddres
+				localIpAddres = getLocalIpAddres()
+				return localIpAddres
 			}
 
 			for _, addr := range addrs {
@@ -55,16 +82,16 @@ func GetPreferredLocalIPv4() string {
 				if ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
 					rs := ipnet.IP.String()
 					if Startwiths(rs, "172.") || Startwiths(rs, "192.") || Startwiths(rs, "10.") {
-						localInternalIpAddres = rs
-						return localInternalIpAddres // 理想情况下是从这里取到
+						localIpAddres = rs
+						return localIpAddres // 理想情况下是从这里取到
 					}
 				}
 			}
 		}
 	}
 
-	localInternalIpAddres = GetLocalIp()
-	return localInternalIpAddres
+	localIpAddres = getLocalIpAddres()
+	return localIpAddres
 }
 
 // 取本机名
