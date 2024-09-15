47fab102997618eab78002e583b60450881fed62
gotoeasy <gotoeasy@163.com>
2024-09-15 21:50:56 +0800

    改善

--- cmn/structure_cache_timeout.go
+++ cmn/structure_cache_timeout.go
@@ -13,17 +13,23 @@ type cacheItem struct {
 
 // 缓存结构
 type Cache struct {
-	mu       sync.RWMutex
-	items    map[string]cacheItem
-	duration time.Duration
+	mu         sync.RWMutex
+	items      map[string]cacheItem
+	duration   time.Duration
+	ignorecase bool
 }
 
 // 新建内存缓存(有存活期，会定期清理失效缓存)
-func NewCache(duration time.Duration) *Cache {
+func NewCache(duration time.Duration, ignorecases ...bool) *Cache {
 	cache := &Cache{
-		items:    make(map[string]cacheItem),
-		duration: duration,
+		items:      make(map[string]cacheItem),
+		duration:   duration,
+		ignorecase: false,
 	}
+	if len(ignorecases) > 0 {
+		cache.ignorecase = ignorecases[0]
+	}
+
 	go cache.cleanupExpired()
 	return cache
 }
@@ -33,11 +39,18 @@ func (c *Cache) Set(key string, value any) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	expiration := time.Now().Add(c.duration)
+	if c.ignorecase {
+		key = ToLower(key)
+	}
 	c.items[key] = cacheItem{value: value, expiration: expiration}
 }
 
 // 获取缓存项
 func (c *Cache) Get(key string) (any, bool) {
+	if c.ignorecase {
+		key = ToLower(key)
+	}
+
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	item, found := c.items[key]
@@ -55,7 +68,38 @@ func (c *Cache) Get(key string) (any, bool) {
 func (c *Cache) Delete(key string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	if c.ignorecase {
+		key = ToLower(key)
+	}
 	delete(c.items, key)
+}
+
+// 取缓存的所有有效期内的键
+func (c *Cache) Keys() []string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var keys []string
+	for key, item := range c.items {
+		if !time.Now().After(item.expiration) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
+}
+
+// 取缓存的所有有效期内的值
+func (c *Cache) Values() []string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var values []string
+	for key, item := range c.items {
+		if !time.Now().After(item.expiration) {
+			values = append(values, key)
+		}
+	}
+	return values
 }
 
 // 每5分钟检查清理一次过期缓存项
