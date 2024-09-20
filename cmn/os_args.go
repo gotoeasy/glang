f2dae699ee45186930f371923b18daad96076546
gotoeasy <gotoeasy@163.com>
2024-09-20 14:32:45 +0800

    args

--- cmn/os_args.go
+++ cmn/os_args.go
@@ -6,24 +6,24 @@ import (
 
 // 命令行解析结果
 type OsArgs struct {
-	String        string // 原命令
-	ArgCount      int    // 参数个数(含命令本身)
-	mapIndexValue map[int]string
-	mapCmd        map[string]bool
-	mapParam      map[string]string
-	LastParam     string // 最后一个参数
-	mapCustomCmd  map[string]bool
+	String        string              // 原命令
+	ArgCount      int                 // 参数个数(含命令本身)
+	LastArg       string              // 最后一个参数
+	mapIndexValue map[int]string      // 内部用
+	mapCmd        map[string]bool     // 内部用
+	mapParam      map[string][]string // 内部用
+	mapCustomCmd  map[string]bool     // 内部用
 }
 
 // 命令行解析器
 // 约定：
-// 参数名总是以“-”作为前缀，参数值紧跟参数名，不支持重复参数名
+// 参数名总是以“-”作为前缀，参数值紧跟参数名
 // 指令默认总是非“-”前缀，但也可以通过参数自定义指令，指令总是忽略大小写
-func ParseArgs(customCmds ...string) *OsArgs {
+func NewOsArgs(customCmds ...string) *OsArgs {
 	args := &OsArgs{}
 	args.mapIndexValue = make(map[int]string)
 	args.mapCmd = make(map[string]bool)
-	args.mapParam = make(map[string]string)
+	args.mapParam = make(map[string][]string)
 	args.mapCustomCmd = make(map[string]bool)
 	args.String = Join(os.Args, " ")
 	args.ArgCount = len(os.Args)
@@ -34,7 +34,7 @@ func ParseArgs(customCmds ...string) *OsArgs {
 
 	for index, arg := range os.Args {
 		args.mapIndexValue[index] = arg
-		args.LastParam = arg // 最后一个参数作为命令的输入看待
+		args.LastArg = arg // 最后一个参数作为命令的输入看待
 	}
 
 	for index, arg := range os.Args {
@@ -58,8 +58,14 @@ func ParseArgs(customCmds ...string) *OsArgs {
 			val := ReplaceAll(arg, "\\r", "\r")
 			val = ReplaceAll(val, "\\n", "\n")
 			val = ReplaceAll(val, "\\t", "\t")
-			args.mapParam[args.mapIndexValue[index-1]] = val
-			args.mapParam["\n"+ToLower(args.mapIndexValue[index-1])] = val
+
+			ary := args.mapParam[args.mapIndexValue[index-1]]
+			if ary == nil {
+				ary = []string{}
+			}
+			ary = append(ary, val)
+			args.mapParam[args.mapIndexValue[index-1]] = ary
+			args.mapParam["\n"+ToLower(args.mapIndexValue[index-1])] = ary
 		}
 	}
 
@@ -70,19 +76,30 @@ func ParseArgs(customCmds ...string) *OsArgs {
 // 例如命令 test -d /abc 用GetArg("-d", "--dir")取得/abc
 func (o *OsArgs) GetArg(names ...string) string {
 	for i := 0; i < len(names); i++ {
-		if o.mapParam[names[i]] != "" {
-			return o.mapParam[names[i]]
+		if o.mapParam[names[i]] != nil {
+			return o.mapParam[names[i]][0] // 多个参数时仅取第一个
 		}
 	}
 	return ""
+}
+
+// 取指定参数名对应的值切片，找不到时返回0长度的切片
+// 例如命令 test -d /abc -d /def 用GetArgs("-d", "--dir")取得["/abc", "/def"]
+func (o *OsArgs) GetArgs(names ...string) []string {
+	for i := 0; i < len(names); i++ {
+		if o.mapParam[names[i]] != nil {
+			return o.mapParam[names[i]] // 多个参数时仅取第一个
+		}
+	}
+	return []string{}
 }
 
 // 取指定参数名对应的值(忽略参数名大小写)
 func (o *OsArgs) GetArgIgnorecase(names ...string) string {
 	for i := 0; i < len(names); i++ {
 		v := o.mapParam["\n"+ToLower(names[i])]
-		if v != "" {
-			return v
+		if v != nil {
+			return v[0] // 多个参数时仅取第一个
 		}
 	}
 	return ""
