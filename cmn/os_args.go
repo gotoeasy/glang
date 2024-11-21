b0a61e5e225b3af373c49f820cd1fb132c26f07e
gotoeasy <gotoeasy@163.com>
2024-11-21 08:28:07 +0800

    win args

--- cmn/os_args.go
+++ cmn/os_args.go
@@ -57,7 +57,7 @@ func NewOsArgs(customCmds ...string) *OsArgs {
 			// 上一个参数是参数，则当前参数是参数值
 			val := arg
 			if IsWin() {
-				val := ReplaceAll(val, "\\r", "\r")
+				val = ReplaceAll(val, "\\r", "\r")
 				val = ReplaceAll(val, "\\n", "\n")
 				val = ReplaceAll(val, "\\t", "\t")
 			}
